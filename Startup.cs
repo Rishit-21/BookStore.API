520e4f11b2a51a9dcbe63fae250b1082b59fe4b2
Rishit-21 <rishit@webtheuniverse.com>
2022-09-30 10:45:51 +0530

    doneee

--- Startup.cs
+++ Startup.cs
@@ -1,18 +1,23 @@
 using BookStore.API.Data;
+using BookStore.API.Models;
 using BookStore.API.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BookStore.API
@@ -32,11 +37,35 @@ namespace BookStore.API
             services.AddDbContext<BookStoreContext>(
                 options => options.UseSqlServer
                 (Configuration.GetConnectionString("BookStoreDB")));
-
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<BookStoreContext>()
+                .AddDefaultTokenProviders();
             services.AddControllers().AddNewtonsoftJson();
             services.AddAutoMapper(typeof(Startup));
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(option=> 
+                {
+                    option.SaveToken = true;
+                    option.RequireHttpsMetadata = false;
+                    option.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience=true,
+                        ValidAudience=Configuration["JWT:ValidAudience"],
+                        ValidIssuer= Configuration["JWT:ValidIssuer"],
+                        IssuerSigningKey= new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
+
+                    };
+                });
+            
 
             services.AddTransient<IBookRepository, BookRepository>();
+            services.AddTransient<IAccountRepository, AccountRepository>();
             //services.AddSwaggerGen(c =>
             //{
             //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookStore.API", Version = "v1" });
@@ -64,7 +93,7 @@ namespace BookStore.API
 
             app.UseRouting();
             app.UseCors();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
